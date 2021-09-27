cbd23c1108a0e01d4dd31e0928c6414b08290f97
Johanna <jccaraballom@gmail.com>
2021-09-26 21:58:43 -0300

    aja

--- Module4-solution/SpeakHello.js
+++ Module4-solution/SpeakHello.js
@@ -11,3 +11,17 @@
     window.helloSpeaker = helloSpeaker;
 
 })(window);
+
+(function (window){
+
+    var speakWord = "Good Bye";
+    var byeSpeaker = {};
+    
+
+    byeSpeaker.speak= function(name) {
+        console.log(speakWord + " " + name);
+    }
+
+    window.byeSpeaker = byeSpeaker;
+
+})(window);
